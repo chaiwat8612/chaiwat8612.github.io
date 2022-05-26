3214d3d766ad07b3aedc8417a2b0c95f67f3980d
chaiwat8612 <89202968+chaiwat8612@users.noreply.github.com>
2022-05-26 13:38:18 +0700

    Update Link

--- hellocr/lib/js/helloaction.js
+++ hellocr/lib/js/helloaction.js
@@ -3,5 +3,6 @@ function hello(){
 }
 
 function openApp(){
-    window.location.replace('hello://cr');
+    // window.location.replace('hello://cr');
+    window.location.replace('https://play.google.com/store/apps/details?id=com.aim.android.umay');
 }
